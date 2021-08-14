85886263220b4874cba8b7fbba075ec661b2b0d2
Himank-Jeshwar <88178218+Himank-Jeshwar@users.noreply.github.com>
2021-08-14 21:46:10 +0530

    Delete 09_finding_Cuberoot.py

--- 09_finding_Cuberoot.py
+++
@@ -1,9 +0,0 @@
-# AUTHOR - HIMANK JESHWAR
-# DATE - 10/08/21
-
-# 1.num takes numbers
-# 2.{n} calculates the cuberoot of {num}
-# 3.last prints the cuberoot
-num=int(input("Enter Number = "))
-n=num**(1/3)
-print(f"Cube root of {num} = {n}")
